81e9d626e720f76ae07988c3d78ee07b3982aec8
Utekishi <w.d.juszczak@gmail.com>
2016-05-11 15:08:31 +0200

    I don't even

---
+++ src/Pickup.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "P:\Oxygine\UML-Game\src\Unit.h"
+
+
+DECLARE_SMART(Pickup, spPickup);
+class Pickup :
+	public Unit
+{
+public:
+	Pickup(std::string type,  int id);
+	~Pickup();
+protected:
+	void _init();
+	void _update(const UpdateState& us);
+	void _die();
+	std::string  _type; //wpn/pup
+	int _id; //with one
+	Vector2 _pos; //where
+
+
+};
+
